6fabc0dd6be6d627485952550a5a8582ada01e51
zmfoq12345 <anihyun1026@naver.com>
2020-05-21 17:01:47 +0900

    Tlqkf

--- app/src/main/java/com/example/gsmin/Main/TitleActivity.java
+++ app/src/main/java/com/example/gsmin/Main/TitleActivity.java
@@ -1,6 +1,8 @@
 package com.example.gsmin.Main;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -8,17 +10,34 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gsmin.R;
 import com.example.gsmin.Splash.SplashActivity;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class TitleActivity extends AppCompatActivity {
 
     ImageButton check_btn, back_btn;
-    private String email;
+    ImageView ec;
+    private static String email = "";
     EditText ed;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +49,7 @@ public class TitleActivity extends AppCompatActivity {
         back_btn = findViewById(R.id.back_btn);
         check_btn = findViewById(R.id.check_btn);
         ed = findViewById(R.id.email_url);
-
+        ec = findViewById(R.id.email_chk);
         back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,13 +61,104 @@ public class TitleActivity extends AppCompatActivity {
         check_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                email = ed.getText().toString()+"@gsm.hs.kr";
-                Log.d("test", "onCreate: "+email);
+                if (ed.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "이메일을 입력하지 않았습니다!", Toast.LENGTH_LONG).show();
+                    return;
+                }
 
+                email = ed.getText().toString() + "@gsm.hs.kr";
+                Log.d("test", "onCreate: " + email);
+
+                ec.setVisibility(View.VISIBLE);
+                check_btn.setVisibility(View.GONE);
                 // if 이메일 인증 성공
-                startActivity(new Intent(getApplication(), InfoActivity.class));
-                TitleActivity.this.finish();
+                JSONTask ResData = (JSONTask) new JSONTask().execute("http://10.53.68.185:3000/emailCheck");//AsyncTask 시작시킴
+
+//                startActivity(new Intent(getApplication(), InfoActivity.class));
+//                TitleActivity.this.finish();
             }
         });
+    }
+
+    public class JSONTask extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.accumulate("email", email);
+//                jsonObject.accumulate("name", "yun");
+
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+                Log.d("asdsad", jsonObject.toString());
+                try{
+                    //URL url = new URL(urls[0]);
+                    //연결을 함
+                    URL url = new URL("http://10.53.68.185:3000/emailCheck");
+                    con = (HttpURLConnection) url.openConnection();
+
+                    con.setRequestMethod("POST");//POST방식으로 보냄
+                    con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
+                    con.setRequestProperty("Content-Type", "application/json");//application JSON 형식으로 전송
+
+
+                    con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
+                    con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
+                    con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
+                    con.connect();
+
+                    //서버로 보내기위해서 스트림 만듬
+                    OutputStream outStream = con.getOutputStream();
+                    //버퍼를 생성하고 넣음
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                    writer.write(jsonObject.toString());
+                    writer.flush();
+                    writer.close();//버퍼를 받아줌
+
+                    //서버로 부터 데이터를 받음
+                    InputStream stream = con.getInputStream();
+
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    StringBuffer buffer = new StringBuffer();
+
+                    String line = "";
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
+
+                    return buffer.toString();//서버로 부터 받은 값을 리턴해줌 아마 OK!!가 들어올것임
+
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                    try {
+                        if(reader != null){
+                            reader.close();//버퍼를 닫아줌
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d("test node", "onPostExecute: "+result);
+        }
     }
 }
