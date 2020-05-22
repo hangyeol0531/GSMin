901f98ac5d81b7d9d3a687d369efb1c8eb7345c4
hangyeol0531 <jhan0531@naver.com>
2020-05-23 02:23:15 +0900

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -1,13 +1,25 @@
 var express = require('express'); //express 모듈 로드
 var bodyParser = require('body-parser');
 var app = express(); // express app 생성
+
+
 const Discord = require('discord.js');
 const nodemailer = require('nodemailer');
 const client = new Discord.Client();
 const config = require('../config.json');
+var port = 3000;
+
 const axios = require('axios');
 const mysql = require('mysql');
-var port = 3000;
+const cors = require('cors');
+
+const School = require('node-school-kr');
+const school = new School()
+school.init(School.Type.HIGH, School.Region.GWANGJU, 'F100000120');
+
+app.use(cors()); 
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 
 var db = mysql.createConnection({
     host: "localhost",
@@ -15,10 +27,6 @@ var db = mysql.createConnection({
     user: "root",
     password: config.dbname
   })
-
-
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
 
 const console_all = async (pr_content) => {
     console.log(pr_content)
@@ -30,43 +38,63 @@ const console_all = async (pr_content) => {
     )
 }
 
+app.post('/gsmschoolfood', async (req, res) =>{
+    console_all("gsmschoolfood 접속")
+    await console_all(`${req.body.dateyear} : ${req.body.datemonth} : ${req.body.dateday}`);
+    await console_all(req.body.eatTime);
 
-app.get('/', (req,res) =>{
+    let meal = await school.getMeal()
+    let meal_info;
+    if(meal.today == "") meal_info = "오늘 급식 정보가 없습니다.";
+    else meal_info = meal.today
+    console_all(meal_info);
 
-});
+    let calendar = await school.getCalendar(req.body.dateyear, req.body.datemonth, req.body.dateday)
+    let cal_info;
+    
+    if(calendar[String(todayday)] = "") cal_info ="오늘 일정 정보가 없습니다." 
+    else cal_info = calendar[String(todayday)]  
+    console_all(cal_info);
+    const send_food_obj = `{'meal' : ${meal_info}, 'cal' : ${cal_info}}, 'kind'x     : "gsmschoolfood"`
+    var send_obj = JSON.parse(send_food_obj);
+    res.end(send_obj)
+})
 
 app.post('/emailCheck', async (req,res)=>{
     await console_all(`${req.body.email} : emailCheck 접속`);
     console.log("이메일 : " + req.body.email);
-
-    const smtpTransport = nodemailer.createTransport({
-        service: "Gmail",
-        auth: {
-            user: config.send_email,
-            pass: config.password
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-      });
-
     
-    const mailOptions = {
-        from: config.send_email,
-        to: req.body.email,
-        subject: "광주소프트웨어마이스터고등학교 GSMin 가입 인증 코드입니다.",
-        text: `GSMin가입에 대한 인증 절차입니다.`
-    };
+    try{
+        const smtpTransport = nodemailer.createTransport({
+            service: "Gmail",
+            auth: {
+                user: config.send_email,
+                pass: config.password
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+          });
     
-    await smtpTransport.sendMail(mailOptions, (error, responses) =>{
-        if(error){
-            res.json({msg:'err'});
-        }else{
-            res.json({msg:'success'});
-        }
-        smtpTransport.close();
-    });
-      
+        
+        const mailOptions = {
+            from: config.send_email,
+            to: req.body.email,
+            subject: "광주소프트웨어마이스터고등학교 GSMin 가입 인증 코드입니다.",
+            text: `GSMin가입에 대한 인증 절차입니다.`
+        };
+        
+        // await smtpTransport.sendMail(mailOptions, (error, responses) =>{
+        //     if(error){
+        //         res.json({msg:'err'});
+        //     }else{
+        //         res.json({msg:'success'});
+        //     }
+        //     smtpTransport.close();
+        // });
+    }catch(e){
+        console.log(e);
+    }
     res.send("connect");
 });
 
