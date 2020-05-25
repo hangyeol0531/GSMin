2ad3c7819a4781cc9e3cbc348bfcf81fadd8e08b
hangyeol0531 <jhan0531@naver.com>
2020-05-26 08:12:27 +0900

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -98,10 +98,27 @@ app.post('/emailCheck', async (req,res)=>{
     res.send("connect");
 });
 
+app.post('/board', async (req,res) =>{
+    console_all("board 접속");
+    var aJsonArray = new Array();
+    var aJson = new Object();
+    for(var i = 0;  i < 10; i++){
+        var aJson = new Object();
+        aJson.likeCount = `${i}6`;
+        aJson.section = "자유";
+        aJson.content = "Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.";
+        aJson.writer = "양현승";
+        aJson.viewer = 32;
+        aJson.previous = `${i}초 전`;
+        console.log(aJson)
+        aJsonArray.push(aJson);
+    }
+    console.log(JSON.stringify(aJsonArray))
+    res.end(JSON.stringify(aJsonArray))
+})
 app.listen(port, function(){
     console.log(`${port}포트로 서버 실행`);
 });
-
 
 //----------------------------------------
 client.on('ready', () => {
