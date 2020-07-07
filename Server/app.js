ba7a8f075e77f6bf7ce9b4d85331c5f789637838
hangyeol0531 <jhan0531@naver.com>
2020-07-07 16:11:30 +0900

    tempory_value

--- Server/app.js
+++ Server/app.js
@@ -15,12 +15,14 @@ const shortId = require('shortId')
 const crypto = require('crypto'); 
 const logger = require('morgan');
 const jwt = require('jsonwebtoken');
+var fs = require('fs');    
 
 const School = require('node-school-kr');
 const school = new School()
 var ip = require('ip');
 const { stringify } = require('querystring');
 const { decode } = require('punycode');
+const { send } = require('process');
 
 school.init(School.Type.HIGH, School.Region.GWANGJU, 'F100000120');
 
@@ -64,7 +66,6 @@ const console_all = async (pr_content) => {
         }
     )
 }
-
 
 // app.post('/token', (req, res) =>{
 //     const payload = {
@@ -217,24 +218,29 @@ app.post('/insert_user_information', (req, res) =>{
 })
 
 app.get('/gsm_hire_list', async (req, res) =>{
-    const job_list = await axios.get('https://sheet.best/api/sheets/a954e6a3-0e4f-48a6-8a20-234ff289fffd');
-    const spread_list = job_list[6]
-    res.end(JSON.stringify(job_list.data.slice(6, -1)));
+    // const job_list = await axios.get('https://sheet.best/api/sheets/a954e6a3-0e4f-48a6-8a20-234ff289fffd');
+    // const spread_list = job_list[6]
+    // res.end(JSON.stringify(job_list.data.slice(6, -1)));
+    var data = fs.readFileSync('./emplist','utf-8');   
+    res.end(data)
 })
 
 app.get('/gsm_employment_rate', async(req, res) =>{
-    const employment_rate = await axios.get('https://sheet.best/api/sheets/a2e05329-b7f6-4824-8295-eaa9ba36e5bd');
-    const employment_rate_data = employment_rate.data.slice(5, -1)
-    let emp_num;
-    console.log(employment_rate_data[1]['2'])
-    for (var i = 0; i < 80; i++){
-        if(!(employment_rate_data[i]['2'])){
-            emp_num = i;
-            break;
-        }
-    }
-    console.log(typeof(emp_num))
-    res.end(String(emp_num * 1.25));
+    // const employment_rate = await axios.get('https://sheet.best/api/sheets/a2e05329-b7f6-4824-8295-eaa9ba36e5bd');
+    // const employment_rate_data = employment_rate.data.slice(5, -1)
+    // let emp_num;
+    // for (var i = 0; i < 80; i++){
+    //     if(!(employment_rate_data[i]['2'])){
+    //         emp_num = i;
+    //         break;
+    //     }
+    // }
+
+    emp_num = 9; // 테스트
+    send_data = new Object;
+    send_data.rate = emp_num * 1.25
+    send_data.emp_num = emp_num
+    res.end(JSON.stringify(send_data));
 })
 app.post('/receive_token_inforation', (req, res) =>{
     try{
