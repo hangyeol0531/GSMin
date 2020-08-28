36fd52fbb9b42ab41cb82174277c3bf2808e4115
hangyeol0531 <jhan0531@naver.com>
2020-08-28 23:41:43 +0900

    Update board.js

--- Server/conf/board.js
+++ Server/conf/board.js
@@ -84,6 +84,7 @@ exports.delete_comment = async(req, res) =>{
 exports.get_board_information = async (req,res) =>{
     fun_all.console_all("get_broad_information 접속");
     console.log(req.body.type, req.body.page_num)
+    req.body.page_num = Number(req.body.page_num);
     var sql = `SELECT * FROM (SELECT * FROM Bulletin_Information WHERE type = "${req.body.type}" ORDER BY date DESC) as A LIMIT 10 OFFSET ${10*(req.body.page_num - 1)} `
     await db.query(sql, function(err, rows){
         if(err) {
