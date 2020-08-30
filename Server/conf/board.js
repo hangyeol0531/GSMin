bd2c1cbdc52730ff2c96735c6d25e40641c23f4b
hangyeol0531 <jhan0531@naver.com>
2020-08-30 22:08:59 +0900

    Update board.js

--- Server/conf/board.js
+++ Server/conf/board.js
@@ -242,7 +242,7 @@ exports.isgood = async (req ,res) => {
                 }
             })
         }else if(check_Code == 1){
-            sql = `DELETE FROM good_board where Bulletin_idx = ${req.body.Bulletin_idx} AND user_email = ${req.body.email};`;
+            sql = `DELETE FROM good_board WHERE Bulletin_idx = ${req.body.Bulletin_idx} AND user_email = "${req.body.email}";`;
             await db.query(sql, (err, rows) =>{
                 if(err){
                     console.log(err);
